e3582b695b72baa601d17c1bb25d7986cb95e5e4
Drakenchef <007kukuru@gmail.com>
2023-09-13 11:13:18 +0300

    delete 3rd GET

--- internal/api/server.go
+++ internal/api/server.go
@@ -20,11 +20,19 @@ func StartServer() {
 		{2, "Сатурн", "Сатурн – газовый гигант, наполненный в основном водородом и гелием. Его размеры позволяют разместить в себе 760 планет типа Земли, а масса больше земной в 95 раз. У Сатурна самая низкая плотность. Осевой оборот Сатурна 10 с половиной часов.", "saturn.jpg", []string{"Вояджер-1", "Вояджер-2", "Пионер-11"}},
 		{3, "Луна", "Луна – пятый по размеру спутник Солнечной системы. Температура поверхности Луны колеблется от −173 °C ночью до +127 °C в подсолнечной точке. Температура пород на глубине 1 метр постоянна и равна −35 °C. Средний радиус Луны составляет 1737,1 километра, то есть примерно 0,273 радиуса Земли.", "moon.jpg", []string{"Аполлон-11", "Аполлон-12", "Аполлон-14"}},
 	}
-
 	r.GET("/", func(c *gin.Context) {
 		r.SetHTMLTemplate(template.Must(template.ParseFiles("./templates/index.html")))
+		searchQuery := c.Query("search")
+
+		filteredPlanets := []backend.Planets{}
+		for _, planet := range Planets {
+			if strings.Contains(strings.ToLower(planet.Name), strings.ToLower(searchQuery)) {
+				filteredPlanets = append(filteredPlanets, planet)
+			}
+		}
+
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"Planets": Planets,
+			"Planets": filteredPlanets,
 		})
 	})
 
@@ -41,20 +49,6 @@ func StartServer() {
 
 		c.HTML(http.StatusOK, "info.html", gin.H{
 			"Planets": selectedPlanet,
-		})
-	})
-	r.GET("/search", func(c *gin.Context) {
-		searchQuery := c.Query("search")
-
-		filteredPlanets := []backend.Planets{}
-		for _, planet := range Planets {
-			if strings.Contains(strings.ToLower(planet.Name), strings.ToLower(searchQuery)) {
-				filteredPlanets = append(filteredPlanets, planet)
-			}
-		}
-
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"Planets": filteredPlanets,
 		})
 	})
 
