69674ff0ee9d30fe7949eeff80478cd3b5b146b0
Drakenchef <007kukuru@gmail.com>
2023-10-16 15:51:19 +0300

    add image work correctly

--- internal/app/handler/planets.go
+++ internal/app/handler/planets.go
@@ -122,33 +122,84 @@ func (h *Handler) createPlanet(planet *ds.Planet) (int, error) {
 	return 0, nil
 }
 
-func (h *Handler) UpdatePlanet(ctx *gin.Context) {
-	var updatedPlanet ds.Planet
-	if err := ctx.BindJSON(&updatedPlanet); err != nil {
-		h.errorHandler(ctx, http.StatusBadRequest, err)
-		return
-	}
-	if updatedPlanet.ID == 0 {
-		h.errorHandler(ctx, http.StatusBadRequest, idNotFound)
-		return
-	}
-	if err := h.Repository.UpdatePlanet(&updatedPlanet); err != nil {
-		h.errorHandler(ctx, http.StatusBadRequest, err)
-		return
-	}
+//func (h *Handler) UpdatePlanet(ctx *gin.Context) {
+//	var updatedPlanet ds.Planet
+//	if err := ctx.BindJSON(&updatedPlanet); err != nil {
+//		h.errorHandler(ctx, http.StatusBadRequest, err)
+//		return
+//	}
+//	if updatedPlanet.ID == 0 {
+//		h.errorHandler(ctx, http.StatusBadRequest, idNotFound)
+//		return
+//	}
+//	if err := h.Repository.UpdatePlanet(&updatedPlanet); err != nil {
+//		h.errorHandler(ctx, http.StatusBadRequest, err)
+//		return
+//	}
+//
+//	h.successHandler(ctx, "updated_planet", gin.H{
+//		"id":          updatedPlanet.ID,
+//		"name":        updatedPlanet.Name,
+//		"description": updatedPlanet.Description,
+//		"radius":      updatedPlanet.Radius,
+//		"distance":    updatedPlanet.Distance,
+//		"gravity":     updatedPlanet.Gravity,
+//		"image":       updatedPlanet.Image,
+//		"type":        updatedPlanet.Type,
+//		"is_delete":   updatedPlanet.IsDelete,
+//	})
+//}
 
-	h.successHandler(ctx, "updated_planet", gin.H{
-		"id":          updatedPlanet.ID,
-		"name":        updatedPlanet.Name,
-		"description": updatedPlanet.Description,
-		"radius":      updatedPlanet.Radius,
-		"distance":    updatedPlanet.Distance,
-		"gravity":     updatedPlanet.Gravity,
-		//"image":       updatedPlanet.Image,
-		"type":      updatedPlanet.Type,
-		"is_delete": updatedPlanet.IsDelete,
-	})
+func (h *Handler) UpdatePlanet(ctx *gin.Context) {
+
+	planetId := ctx.Request.FormValue("id")
+	planetName := ctx.Request.FormValue("name")
+	description := ctx.Request.FormValue("description")
+	radius := ctx.Request.FormValue("radius")
+	distance := ctx.Request.FormValue("distance")
+	gravity := ctx.Request.FormValue("gravity")
+	types := ctx.Request.FormValue("type")
+	planetIduint, _ := strconv.Atoi(planetId)
+	radiusfloat, _ := strconv.ParseFloat(radius, 64)
+	distancefloat, _ := strconv.ParseFloat(distance, 64)
+	gravityfloat, _ := strconv.ParseFloat(gravity, 64)
+
+	newPlanet := ds.Planet{
+		ID:          uint(planetIduint),
+		Name:        planetName,
+		IsDelete:    false,
+		Description: description,
+		Radius:      radiusfloat,
+		Distance:    distancefloat,
+		Gravity:     gravityfloat,
+		Type:        types,
+	}
+	file, header, _ := ctx.Request.FormFile("image")
+	if errorCode, errCreate := h.updatePlanet(&newPlanet); errCreate != nil {
+		h.errorHandler(ctx, errorCode, errCreate)
+	}
+	if file != nil && header.Size != 0 && header != nil {
+		newImageURL, errCode, errDB := h.createImagePlanet(&file, header, fmt.Sprintf("%d", newPlanet.ID))
+		if errDB != nil {
+			h.errorHandler(ctx, errCode, errDB)
+			return
+		}
+		newPlanet.Image = newImageURL
+	}
+	ctx.Redirect(http.StatusFound, "/Planets")
+
 }
+
+func (h *Handler) updatePlanet(planet *ds.Planet) (int, error) {
+	if planet.ID == 0 {
+		return http.StatusBadRequest, idNotFound
+	}
+	if err := h.Repository.UpdatePlanet(planet); err != nil {
+		return http.StatusBadRequest, err
+	}
+	return 0, nil
+}
+
 func (h *Handler) AddImage(ctx *gin.Context) {
 	file, header, err := ctx.Request.FormFile("image")
 	planetID := ctx.Request.FormValue("id")
